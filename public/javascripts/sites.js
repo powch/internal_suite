171810e833a53f6559bf3145bb49669625ba8139
Chace Powell <cpcpowell8@gmail.com>
2018-09-26 00:42:27 -0400

    add sites file and begin building out file. TODO: complete sites starting from Other Tools.

---
+++ public/javascripts/sites.js
@@ -0,0 +1,102 @@
+const siteArray = [
+    {
+        siteName: 'Compass',
+        siteURL: '',
+        category: 'ticketing'
+    },
+    {
+        siteName: 'Ticket Watcher',
+        siteURL: '',
+        category: 'ticketing'
+    },
+    {
+        siteName: 'Ticket Archive',
+        siteURL: '',
+        category: 'ticketing'
+    },
+    {
+        siteName: 'Tickets',
+        siteURL: '',
+        category: 'ticketing'
+    },
+    {
+        siteName: 'Assets',
+        siteURL: '',
+        category: 'inventory'
+    },
+    {
+        siteName: 'CyberArk',
+        siteURL: '',
+        category: 'inventory'
+    },
+    {
+        siteName: 'EM7 Dashboard',
+        siteURL: '',
+        category: 'inventory'
+    },
+    {
+        siteName: 'WASP',
+        siteURL: '',
+        category: 'inventory'
+    },
+    {
+        siteName: 'Hardware Tracking',
+        siteURL: '',
+        category: 'inventory'
+    },
+    {
+        siteName: 'CM MOPS',
+        siteURL: '',
+        category: 'projects'
+    },
+    {
+        siteName: 'Customer MOPS',
+        siteURL: '',
+        category: 'projects'
+    },
+    {
+        siteName: 'Trello',
+        siteURL: '',
+        category: 'projects'
+    },
+    {
+        siteName: 'Project Insight',
+        siteURL: '',
+        category: 'projects'
+    },
+    {
+        siteName: 'Cust. CrossConnect Form',
+        siteURL: '',
+        category: 'projects'
+    },
+    {
+        siteName: 'CrossConnect Express Form',
+        siteURL: '',
+        category: 'projects'
+    },
+    {
+        siteName: 'Expense Reporting',
+        siteURL: '',
+        category: 'personal'
+    },
+    {
+        siteName: 'Paycom',
+        siteURL: '',
+        category: 'personal'
+    },
+    {
+        siteName: 'Signature Tool',
+        siteURL: '',
+        category: 'personal'
+    },
+    {
+        siteName: 'Outlook Web Access',
+        siteURL: '',
+        category: 'personal'
+    },
+    {
+        siteName: 'Cisco Phone Management',
+        siteURL: '',
+        category: 'personal'
+    }
+]
